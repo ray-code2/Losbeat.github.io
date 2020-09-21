505f600f2c2dbcd6c4df642a1d7ad2697274b64b
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2020-09-21 11:14:52 +0700

    Delete signup.js

--- signup.js
+++
@@ -1,5 +0,0 @@
-const Username = document.getElementById('Username');
-const Email = document.getElementById('Email');
-const Password = document.getElementById('Password');
-const Confpass = document.getElementById('Confirm Password');
-const form = document.getElementById('form');
