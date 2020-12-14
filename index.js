8462cda32bed5736e63e2b660b89ef2f521e4ef2
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2020-12-14 14:58:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./models/user');
+const login = require('./models/login');
 const app = express();
 const port = process.env.PORT || 3000;
 const mydb = 'mongodb+srv://evorius:123@clusterv-1.bdlq1.mongodb.net/users?retryWrites=true&w=majority';
 mongoose.connect(mydb , {useNewUrlParser:true , useUnifiedTopology:true})
 .then(result => {app.listen(port, () => {
-   console.log(`Server started on port 3000`);
+   console.log(`Server started on port ${port}`);
 });})
 .catch(err => console.log(err))
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
-//
+
 // Losbeat Landing Page
 app.get('/' , (req , res) =>{
    res.sendFile('./Views/Losbeats.html' , {root: __dirname});
@@ -25,7 +26,19 @@ app.get('/create' , (req , res) =>{
  })
 // Losbeat Login Page
  app.get('/login' , (req , res) => {
-   res.sendFile('./Views/Join.html' , {root: __dirname})
+   login.find()
+   .then((result) => res.sendFile('./Views/Join.html' , {root: __dirname}))
+   .catch((err) => console.log(err))
+})
+app.post('/login' , (req , res) => {
+   const logins = new login(req.body);
+   logins.save()
+   .then((result) => {
+        res.redirect('/login');
+   })
+   .catch((err) => {
+       console.log(err)
+   })
 })
 // Losbeat Family PAge
 app.get('/family' , (req , res) => {
